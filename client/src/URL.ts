1fd249668c323e1c9cc1df6cdd35e6f4d00d7e58
EricSeall <14873348+EricSeall@users.noreply.github.com>
2024-10-07 13:32:37 -0500

    fix typo in server url for client

--- client/src/URL.ts
+++ client/src/URL.ts
@@ -1,4 +1,4 @@
 //  Development Environment:
 //  export const URL = "http://localhost:4000";
 //  export const URL = "https://project-tracker-server-l2fs.onrender.com";
-export const URL = "https://server.aceuramusic.com";
+export const URL = "https://server.aceauramusic.com";
