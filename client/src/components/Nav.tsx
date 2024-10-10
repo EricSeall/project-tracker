0903061a4ab8ff03a9dd5ff87ede8b21e3411a40
EricSeall <14873348+EricSeall@users.noreply.github.com>
2024-10-10 13:43:33 -0500

    add favicon and make Log Out button have pointer

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -5,37 +5,44 @@ import DarkIcon from "./icons/DarkIcon";
 import LightIcon from "./icons/LightIcon";
 
 interface Props {
-	title: String;
-	toggleDarkMode: Function;
-	darkMode: boolean;
+  title: String;
+  toggleDarkMode: Function;
+  darkMode: boolean;
 }
 
 export default function Nav(props: Props) {
-	const { title, toggleDarkMode, darkMode } = props;
+  const { title, toggleDarkMode, darkMode } = props;
 
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	function logOut() {
-		Cookies.remove("token");
-		Cookies.remove("refresh_token");
-		navigate("/");
-	}
+  function logOut() {
+    Cookies.remove("token");
+    Cookies.remove("refresh_token");
+    navigate("/");
+  }
 
-	return (
-		<>
-			<Navbar isBlurred className="flex items-center justify-center text-content1">
-				<NavbarContent justify="center">
-					<h1 className="text-2xl font-bold">{title}</h1>
-				</NavbarContent>
-				<NavbarContent justify="center">
-					<Button isIconOnly className="bg-transparent" onClick={() => toggleDarkMode()}>
-						{darkMode ? <LightIcon /> : <DarkIcon />}
-					</Button>
-					<a className="font-semibold" onClick={logOut}>
-						Log Out
-					</a>
-				</NavbarContent>
-			</Navbar>
-		</>
-	);
+  return (
+    <>
+      <Navbar
+        isBlurred
+        className="flex items-center justify-center text-content1"
+      >
+        <NavbarContent justify="center">
+          <h1 className="text-2xl font-bold">{title}</h1>
+        </NavbarContent>
+        <NavbarContent justify="center">
+          <Button
+            isIconOnly
+            className="bg-transparent"
+            onClick={() => toggleDarkMode()}
+          >
+            {darkMode ? <LightIcon /> : <DarkIcon />}
+          </Button>
+          <a className="font-semibold cursor-pointer" onClick={logOut}>
+            Log Out
+          </a>
+        </NavbarContent>
+      </Navbar>
+    </>
+  );
 }
