349ec62752fa4b0e255ad33303b79bd95adc12e9
EricSeall <14873348+EricSeall@users.noreply.github.com>
2024-11-21 11:00:39 -0600

    set theme-color for Safari iOS

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,58 +10,62 @@ import Cookies from "js-cookie";
 import { useState } from "react";
 
 function App() {
-  const [darkMode, setDarkMode] = useState(true);
+	const [darkMode, setDarkMode] = useState(true);
 
-  function toggleDarkMode() {
-    setDarkMode(!darkMode);
-  }
+	function toggleDarkMode() {
+		const themeColor = document.querySelector('meta[name="theme-color"]');
+		if (themeColor && themeColor.getAttribute("content") == "#0e0920") {
+			themeColor.setAttribute("content", "#d8dbe2");
+		} else if (themeColor) {
+			themeColor.setAttribute("content", "#0e0920");
+		}
 
-  return (
-    <BrowserRouter>
-      <div className={`app${darkMode ? " dark" : ""} bg-background`}>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              Cookies.get("refresh_token") ? (
-                <Projects darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
-              ) : (
-                <Login />
-              )
-            }
-          />
-          <Route
-            path="/projects"
-            element={
-              <PrivateRoute>
-                <Projects darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/projects/:id"
-            element={
-              <PrivateRoute>
-                <ProjectDetail
-                  darkMode={darkMode}
-                  toggleDarkMode={toggleDarkMode}
-                />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/create"
-            element={
-              <PrivateRoute>
-                <Create darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
-              </PrivateRoute>
-            }
-          />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
+		setDarkMode(!darkMode);
+	}
+
+	return (
+		<BrowserRouter>
+			<div className={`app${darkMode ? " dark" : ""} bg-background`}>
+				<Routes>
+					<Route
+						path="/"
+						element={
+							Cookies.get("refresh_token") ? (
+								<Projects darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+							) : (
+								<Login />
+							)
+						}
+					/>
+					<Route
+						path="/projects"
+						element={
+							<PrivateRoute>
+								<Projects darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+							</PrivateRoute>
+						}
+					/>
+					<Route
+						path="/projects/:id"
+						element={
+							<PrivateRoute>
+								<ProjectDetail darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+							</PrivateRoute>
+						}
+					/>
+					<Route
+						path="/create"
+						element={
+							<PrivateRoute>
+								<Create darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+							</PrivateRoute>
+						}
+					/>
+					<Route path="*" element={<NotFound />} />
+				</Routes>
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
